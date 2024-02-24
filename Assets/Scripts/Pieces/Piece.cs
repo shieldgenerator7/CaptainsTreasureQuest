02c1b3ad95ce94daa8e5466dfe3cefd740085aa2
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-02-24 00:09:19 -0500

    Update piece

--- Assets/Scripts/Pieces/Piece.cs
+++ Assets/Scripts/Pieces/Piece.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Piece", menuName = "Piece/Piece")]
 public class Piece : ScriptableObject
 {
-    
+    [Header("Move Patterns")]
+    public MovePattern movePattern;
+    public MovePattern capturePattern;
+    public MovePattern shootPattern;
+    public MovePattern visionPattern;
+    public MovePattern detectPattern;
+    public MovePattern digPattern;
+
 }

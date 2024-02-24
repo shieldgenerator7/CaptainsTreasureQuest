30fd51841a078342a6814bb962234b8d4b47db39
shieldgenerator7 <shieldgenerator7@yahoo.com>
2024-02-24 11:45:07 -0500

    Seal GestureProfile

--- Assets/Scripts/GestureProfile.cs
+++ Assets/Scripts/GestureProfile.cs
@@ -2,26 +2,27 @@
 using System.Collections;
 using System;
 
-public class GestureProfile
+public sealed class GestureProfile
 {//2018-01-22: copied from Stonicorn.GestureProfile
 
     /// <summary>
     /// Called when this profile is set to the current one,
     /// or when the GestureManager switches off this profile to a different one
     /// </summary>
-    public virtual void activate(bool active) {
+    public void activate(bool active)
+    {
         OnActivatedChanged?.Invoke(active);
     }
     public Action<bool> OnActivatedChanged;
 
-    public virtual void processTapGesture(Vector3 curMPWorld)
-    {        
+    public void processTapGesture(Vector3 curMPWorld)
+    {
         OnTap?.Invoke(curMPWorld);
     }
     public Action<Vector3> OnTap;
 
-    public virtual void processHoldGesture(Vector3 curMPWorld, float holdTime, bool finished)
-    {        
+    public void processHoldGesture(Vector3 curMPWorld, float holdTime, bool finished)
+    {
         OnHold?.Invoke(curMPWorld, holdTime, finished);
     }
     public Action<Vector3, float, bool> OnHold;
@@ -32,13 +33,13 @@ public class GestureProfile
     }
     public Action<Vector3, Vector3> OnDrag;
 
-    public virtual void processPinchGesture(int adjustment)
+    public void processPinchGesture(int adjustment)
     {
         OnPinch?.Invoke(adjustment);
     }
     public Action<int> OnPinch;
 
-    public virtual void processCursorMoveGesture(Vector3 curMPWorld, bool show)
+    public void processCursorMoveGesture(Vector3 curMPWorld, bool show)
     {
         OnCursorMove?.Invoke(curMPWorld, show);
     }
